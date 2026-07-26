e1bcf58829cb64aba4d26560c82bc044c3354f61
Kris Lee <kdlee0401@gmail.com>
2026-07-25 22:24:15 -0500

    FINISHED THE BODY OF THE WEB

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Kris Lee | July 24, 2026
+"use strict";
+
+const SONG_LIST = {
+    song1: "assets/audio/Ax and the Hatchetmen- MAKO.mp3",
+    song2: "assets/audio/beabadoobee - 'Cologne'.mp3",
+    song3: "assets/audio/keshi - Forever (Official Visualizer).mp3",
+    song4: "assets/audio/OXYGEN.mp3"
+};
+
+for (let key in SONG_LIST) {
+    console.log(key);
+    console.log(`Song name: ${SONG_LIST[key]}`);
+};
